c597e5f455ed34246c7f25c665c8db18ad93ca98
Ezra Wang <zwang24@tfs.ca>
2023-01-09 10:27:48 -0500

    INitial commit

---
+++ app/src/main/java/com/example/catalog3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.catalog3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//premier essaie
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
