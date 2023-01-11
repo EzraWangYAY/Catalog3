475fb66515f38573dc56979ad6b2cfbd2d5bc4c8
Ezra Wang <zwang24@tfs.ca>
2023-01-11 11:18:40 -0500

    Premiere Partie

--- app/src/main/java/com/example/catalog3/Activity/MainActivity.java
+++ app/src/main/java/com/example/catalog3/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.catalog3;
+package com.example.catalog3.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.catalog3.R;
 
 public class MainActivity extends AppCompatActivity {
 //premier essaie
---
+++ app/src/main/java/com/example/catalog3/Model/Flights.java
@@ -0,0 +1,132 @@
+package com.example.catalog3.Model;
+
+public class Flights {
+    private String id;
+    private String iata;
+    private String icao;
+    private String name;
+    private String location;
+    private String county;
+    private String state;
+    private String country_iso;
+    private String country;
+    private String postal_code;
+    private String phone;
+    private String latitude;
+    private String longitude;
+    private String website;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIata() {
+        return iata;
+    }
+
+    public void setIata(String iata) {
+        this.iata = iata;
+    }
+
+    public String getIcao() {
+        return icao;
+    }
+
+    public void setIcao(String icao) {
+        this.icao = icao;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry_iso() {
+        return country_iso;
+    }
+
+    public void setCountry_iso(String country_iso) {
+        this.country_iso = country_iso;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+
+}
