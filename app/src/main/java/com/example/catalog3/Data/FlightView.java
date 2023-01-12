45b693e568e259b149489b9f49cb0456269e0ef7
Ezra Wang <zwang24@tfs.ca>
2023-01-12 09:45:55 -0500

    Deuxieme Partie

--- app/src/main/java/com/example/catalog3/Data/FlightView.java
+++ app/src/main/java/com/example/catalog3/Data/FlightView.java
@@ -8,16 +8,20 @@ import android.annotation.NonNull;
 
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.catalog3.R;
 import com.example.catalog3.Model.Flights;
+import com.squareup.picasso.Picasso;
 
 
 public class FlightView extends RecyclerView.Adapter<FlightRecyclerView>.ViewHolder {
     private Context context;
     private List<Flight> flightList;
+
 public FlightView(Context context, List<Flight> flight){
     this.context = context;
     flightList = flight;
@@ -27,30 +31,55 @@ public FlightView(Context context, List<Flight> flight){
     @NonNull
     @Override
     public FlightView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-    View views LayoutInflater.from(parent.getContext()).inflate(R.layout.flight,parent, false);
-    return new ViewHolder(view.context);
+    View views = LayoutInflater.from(parent.getContext()).inflate(R.layout.airport,parent, false);
 
-        return null;
+    return new ViewHolder(views, context);
+
+
 
 
     }
     @Override
     public void onBindViewHolder(@NonNull FlightView.ViewHolder holder, int position){
+    Flights flight = flightList.get(position);
+    String airSnapShot = flight.getSnapShot();
+    holder.name.setText(flight.getName());
+    holder.location.setText(flight.getLocation());
+    holder.phone.setText(flight.getPhone());
+    Picasso.get()
+            .load(airSnapShot)
+            .fit()
+            .placeholder(android.R.drawable.ic_btn_speak_now)
+            .into(holder.airSnap);
+
 
     }
     @Override
     public int getItemCount(){
-        return 0;
+        return flightList.size();
 
     }
-    public class viewHolder extends RecyclerView.ViewHolder{
-        public ViewHolder(@NonNull View itemView){
+    public class ViewHolder extends RecyclerView.ViewHolder{
+    TextView name;
+    TextView location;
+    TextView phone;
+    ImageView airSnap;
+        public ViewHolder(@NonNull View itemView, Context context){
             super(itemView);
+            context=context;
+            name=itemView.findViewById(R.id.flightNameID);
+            airSnap=itemView.findViewById(R.id.flightLocationID);
+            location=itemView.findViewById(R.id.flightLocationID);
+            phone=itemView.findViewById(R.id.flightPhoneID);
+
+
 
         }
     }
 
 
-    public class ViewHolder {
+    public class ViewHolder1 {
+        public ViewHolder1(View views, Context context) {
+        }
     }
 }
